8f6fa4b4ed5dd66ca17fcddd446681d61b39e212
Michel Vieira <152667600+micheldrv@users.noreply.github.com>
2025-02-03 15:48:29 +0000

    Implement the getComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3);
+  switch (random) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
 window.onload = () => {
   console.log("hello");
 };
